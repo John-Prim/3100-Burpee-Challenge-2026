5e91c1d5daa5338fe8063e6c5f674f84a831dbf7
John-Prim <j.prim.3908@gmail.com>
2026-02-07 20:43:20 -0800

    Create contest.ts

---
+++ lib/contest.ts
@@ -0,0 +1,5 @@
+export const CONTEST_START = "2026-03-01";
+export const CONTEST_END = "2026-03-31";
+export const MARCH_DAYS = 31;
+export const DAILY_GOAL = 100;
+export const MONTH_GOAL = MARCH_DAYS * DAILY_GOAL; // 3100
